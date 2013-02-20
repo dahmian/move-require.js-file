e009ea1168555a1fceed006be410aa07640f8ac7
Dahmian Owen <dahmian.owen@mellmo.com>
2013-02-20 12:53:11 -0800

    searching and replacing for require format

--- recursive-replace.js
+++ recursive-replace.js
@@ -1,9 +1,15 @@
-var rootPath = process.argv[2];
-var searchString = process.argv[3];
-var replaceString = process.argv[4];
+var rootPath = "Resources/js/";
+
+searchString = convertToRequireFormat(process.argv[2]);
+replaceString = convertToRequireFormat(process.argv[3]);
 
 var iterateThroughJs = require("./iterate-through-js-files.js").iterateThroughJs;
 iterateThroughJs(rootPath, replaceInFile);
+
+function convertToRequireFormat(path) {
+  path = path.replace(rootPath, "");
+  return path.replace(".js", "");
+}
 
 function replaceInFile(path) {
   var replace = require("./replaceInFile.js");
